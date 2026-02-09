b3bf665f58290a957fd6def76f98c58e1b6e9186
Surajixcp <surajgiri2002x@gmail.com>
2026-02-09 19:53:59 +0530

    new

--- admin-control-panel/services/api.ts
+++ admin-control-panel/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://matkaking-satta.onrender.com/api/v1';
+const API_URL = import.meta.env.VITE_API_URL || 'https://matkaking-satta-1.onrender.com/api/v1';
 
 const api = axios.create({
     baseURL: API_URL,

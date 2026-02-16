5fc0a75e9274d3467ff77d61566bc68e7f6affc9
Surajixcp <surajgiri2002x@gmail.com>
2026-02-16 12:29:47 +0530

    new update

--- backend/src/app/routes/results.routes.js
+++ backend/src/app/routes/results.routes.js
@@ -3,9 +3,7 @@ const router = express.Router();
 const resultsController = require('../controllers/results.controller');
 const { protect, authorize } = require('../middlewares/auth.middleware');
 
-router.post('/', protect, authorize('admin'), resultsController.declareResult);
-router.get('/', protect, authorize('admin'), resultsController.getHistory);
-router.post('/reprocess', protect, authorize('admin'), resultsController.reprocessResults);
+router.get('/', protect, resultsController.getHistory);
 router.delete('/:id', protect, authorize('admin'), resultsController.revokeResult);
 
 module.exports = router;
